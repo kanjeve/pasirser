7976664a15681ed0999535501227fb3f1b8de8c2
Kanji Hirata <s2520148@u.tsukuba.ac.jp>
2025-11-14 11:20:58 +0900

    gitignore

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export { analyze } from './cli/cli.js';
-export * from './core/ast/asirAst.js';
-export { AsirASTBuilder } from './core/ast/asirASTBuilder.js';
-export { CustomErrorListener } from './core/parser/customErrorListener.js';
-export type { SyntaxErrorInfo, AmbiguityInfo, DiagnosticInfo } from './core/parser/customErrorListener.js';
-export { ASTBuilderError } from './utils/errors.js';
-export { Diagnostic, DiagnosticSeverity } from './utils/diagnostics.js';
-export { SymbolTable } from './semantics/symbolTable.js';
-export { Symbol, AsirType, Scope } from './semantics/types.js';
